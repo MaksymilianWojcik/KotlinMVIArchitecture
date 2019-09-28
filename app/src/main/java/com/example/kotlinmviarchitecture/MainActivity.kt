94cf9f3162d00a10a2693e03221c4f2c5e193bec
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-09-28 18:33:59 +0200

    Added main fragment

--- app/src/main/java/com/example/kotlinmviarchitecture/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmviarchitecture/MainActivity.kt
@@ -9,4 +9,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun showMainFragment(){
+        val mainFragment:MainFragment = MainFragment();
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, mainFragment, "MainFragment")
+            .commit();
+    }
+
 }
